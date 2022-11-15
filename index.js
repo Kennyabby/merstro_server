61fb3e51ea94d9a3d051fab5d4c671b247ad39aa
kennyabby <kennyabby22@gmail.com>
2022-11-15 06:27:58 +0100

    new changes

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:' + port + '/auth/google/callback',
+      callbackURL: '/auth/google/callback',
     },
     function (accessToken, refreshToken, profile, done) {
       userProfile = profile
